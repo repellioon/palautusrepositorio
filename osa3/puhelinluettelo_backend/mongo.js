a4cde506e9abe103180b4854501321f0854fcaee
reevirta <reetta.p.virtanen@student.jyu.fi>
2026-03-11 10:03:03 +0200

    tehtävä 3.12.

---
+++ osa3/puhelinluettelo_backend/mongo.js
@@ -0,0 +1,48 @@
+const mongoose = require('mongoose')
+
+if (process.argv.length < 3) {
+  console.log('give password as argument')
+  process.exit(1)
+}
+
+const password = process.argv[2]
+
+const url = `mongodb+srv://reettapvirtanen_db_user:${password}@cluster0.k1uma3l.mongodb.net/phonebook?retryWrites=true&w=majority&appName=Cluster0`
+
+mongoose.set('strictQuery', false)
+mongoose.connect(url, { family: 4 })
+
+const personSchema = new mongoose.Schema({
+  name: String,
+  number: String,
+})
+
+// Luodaan malli
+const Person = mongoose.model('Person', personSchema)
+
+// Jos annettu vain salasana, listataan kaikki henkilöt
+if (process.argv.length === 3) {
+  Person.find({}).then(result => {
+    console.log('phonebook:')
+    result.forEach(person => {
+      console.log(`${person.name} ${person.number}`)
+    })
+    mongoose.connection.close()
+  })
+}
+
+// Jos annettu salasana, nimi ja numero, lisätään uusi henkilö
+if (process.argv.length === 5) {
+  const name = process.argv[3]
+  const number = process.argv[4]
+
+  const person = new Person({
+    name: name,
+    number: number,
+  })
+
+  person.save().then(() => {
+    console.log(`added ${name} number ${number} to phonebook`)
+    mongoose.connection.close()
+  })
+}

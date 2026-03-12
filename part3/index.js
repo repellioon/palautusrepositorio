21feb3f81ae0b00f4a88766a4061c89726a33ff7
reevirta <reetta.p.virtanen@student.jyu.fi>
2026-03-12 09:31:30 +0200

    harjoitustehtävät 3C

--- part3/index.js
+++ part3/index.js
@@ -15,13 +15,9 @@ const requestLogger = (request, response, next) => {
   next()
 }
 
+app.use(express.static('dist'))
 app.use(express.json())
 app.use(requestLogger)
-app.use(express.static('dist'))
-
-app.get('/', (request, response) => {
-  response.send('<h1>Hello World!</h1>')
-})
 
 //3C osan harjoitukset
 app.get('/api/notes', (request, response) => {
@@ -32,11 +28,44 @@ app.get('/api/notes', (request, response) => {
   })
 })
 
-//yksittäisen muistiinpanon tarkastelu, 3C
-app.get('/api/notes/:id', (request, response) => {
-  Note.findById(request.params.id).then(note => {
-    response.json(note)
-  })
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: 'unknown endpoint' })
+}
+
+// olemattomien osoitteiden käsittely
+app.use(unknownEndpoint)
+
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message)
+
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' })
+  }
+
+  next(error)
+}
+
+// virheellisten pyyntöjen käsittely
+app.use(errorHandler)
+
+app.get('/', (request, response) => {
+  response.send('<h1>Hello World!</h1>')
+})
+
+
+//yksittäisen muistiinpanon tarkastelu, 3C ja D virheen käsittely
+app.get('/api/notes/:id', (request, response, next) => {
+  Note.findById(request.params.id)
+    .then(note => {
+
+      if (note) {
+        response.json(note)
+      } else {
+        response.status(404).end()
+      }
+    })
+
+    .catch(error => next(error))
 })
 
 /*const generateId = () => {
@@ -63,18 +92,33 @@ app.post('/api/notes', (request, response) => {
   })
 })
 
-app.delete('/api/notes/:id', (request, response) => {
-  const id = request.params.id
-  notes = notes.filter((note) => note.id !== id)
+// yksittäisen muistiinpanon muokkaaminen
+app.put('/api/notes/:id', (request, response, next) => {
+  const { content, important } = request.body
 
-  response.status(204).end()
+  Note.findById(request.params.id)
+    .then(note => {
+      if (!note) {
+        return response.status(404).end()
+      }
+
+      note.content = content
+      note.important = important
+
+      return note.save().then((updatedNote) => {
+        response.json(updatedNote)
+      })
+    })
+    .catch(error => next(error))
 })
 
-const unknownEndpoint = (request, response) => {
-  response.status(404).send({ error: 'unknown endpoint' })
-}
-
-app.use(unknownEndpoint)
+app.delete('/api/notes/:id', (request, response, next) => {
+  Note.findByIdAndDelete(request.params.id)
+    .then(result => {
+      response.status(204).end()
+    })
+    .catch(error => next(error))
+})
 
 const PORT = process.env.PORT
 app.listen(PORT, () => {

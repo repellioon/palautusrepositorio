4c53d152b53d44a898c0e6e2aa9abfb620639eb2
reevirta <reetta.p.virtanen@student.jyu.fi>
2026-03-18 09:40:22 +0200

    frontend yhdistetty backendiin osa3

---
+++ osa3/puhelinluettelo_backend/frontend/src/components/Notification.jsx
@@ -0,0 +1,14 @@
+const Notification = ({ message, type }) => {
+  if (!message) {
+    return null
+  }
+
+  return (
+    <div className={type === 'error' ? 'error' : 'success'}>
+      {message}
+    </div>
+  )
+}
+
+
+export default Notification

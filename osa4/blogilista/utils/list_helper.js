35a96332fb7f128061ac39ff83abcbf3437b8d51
reevirta <reetta.p.virtanen@student.jyu.fi>
2026-03-19 09:59:48 +0200

    tehtävä 4.3. valmis, testit

---
+++ osa4/blogilista/utils/list_helper.js
@@ -0,0 +1,28 @@
+const dummy = (blogs) => {
+    
+    const bloglist = [
+        {
+            title: "coding is fun",
+            author: "Reetta V",
+            url: "www.reettav.fi",
+            likes: 5
+        },
+                {
+            title: "cooking is fun",
+            author: "Pirjo",
+            url: "www.pirjo.fi",
+            likes: 2
+        },
+                {
+            title: "where to eat",
+            author: "Misso",
+            url: "www.misso.fi",
+            likes: 3
+        }
+    ]
+    return 1
+}
+
+module.exports = {
+    dummy
+}
